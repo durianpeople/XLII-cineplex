6b17d1b829fe37e72033d54141935f76ad57307b
durianpeople <durianpeople@gmail.com>
2018-12-12 15:40:04 +0700

    Update resources/views/pages/film/daftar.blade.php

--- resources/views/pages/film/daftar.blade.php
+++ resources/views/pages/film/daftar.blade.php
@@ -5,7 +5,7 @@
 Film yang tersedia:<br>
 <ul>
 	@foreach(Film::all() as $film)
-		<li>{{$film->nama_film}}</li>
+		<li>{{$film->id_film}}: {{$film->nama_film}}</li>
 	@endforeach
 </ul>
 @endsection
