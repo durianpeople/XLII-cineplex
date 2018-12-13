1910c77eaf50924c4f4d6d142e147679915d7d3a
durianpeople <durianpeople@gmail.com>
2018-12-13 21:12:44 +0700

    tes cek film tayang apa ga

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -9,10 +9,10 @@ foreach(DetilPemutaran::all() as $d) {
 foreach(Film::all() as $d) {
 	echo "Film: ".$d->nama_film."<br>";
 	echo "ID film: ".$d->getAttribute('id_film')."<br>";
-	echo "Status: ".(DetilPemutaran::where('id_film',$d->id_film)->get());
+	$result=DetilPemutaran::where('id_film',$d->id_film)->get();
+	if (!$result->isEmpty()) echo "tayang";
+	else echo "ga tayang";
+	echo "<br>";
 }
 
-foreach(Film::with('detilpemutaran')->get() as $d) {
-	var_dump($d);
-}
 ?>
