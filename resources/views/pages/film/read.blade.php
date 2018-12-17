87958e53f72d9a1c6caa1516586ece87fcb464de
Axerion1210 <octavianusgiovanni@gmail.com>
2018-12-17 15:40:05 +0700

    minor edit

--- resources/views/pages/film/read.blade.php
+++ resources/views/pages/film/read.blade.php
@@ -20,7 +20,6 @@
             @endforeach
           </small>
           <br><small> Durasi: {{$film->durasi}} menit</small>
-          <h4 style="margin-top:20px;">Harga Rp{{$harga->harga_per_tiket}}.00</h4>
           <p class="card-text">
                 <div class="d-block my-3">
                    
