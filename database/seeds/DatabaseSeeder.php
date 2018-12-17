796b19422bfbf6b164dcfd49a8e3603872508c01
Axerion1210 <octavianusgiovanni@gmail.com>
2018-12-17 19:15:49 +0700

    kurang include studio seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             GenreSeeder::class,
             FilmGenreSeeder::class,
             TransaksiSeeder::class,
+            StudioSeeder::class,
         ]);
     }
 }
