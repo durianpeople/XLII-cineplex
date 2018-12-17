89290ab2f5f317894baadaa19508936011cbb617
Axerion1210 <octavianusgiovanni@gmail.com>
2018-12-17 23:03:08 +0700

    koreksi transaksi

--- database/seeds/TransaksiSeeder.php
+++ database/seeds/TransaksiSeeder.php
@@ -15,11 +15,13 @@ class TransaksiSeeder extends Seeder
             'id_putar' => 1,
             'id_user' => 2,
             'jumlah_tiket' => 10,
+            'harga_total' => 450000,
         ]);
         DB::table('transaksis')->insert([
             'id_putar' => 4,
             'id_user' => 2,
             'jumlah_tiket' => 4,
+            'harga_total' => 90000,
         ]);
     }
 }
--- resources/views/pages/admin/transaksi.blade.php
+++ resources/views/pages/admin/transaksi.blade.php
@@ -15,20 +15,15 @@ use App\Transaksi;
             </tr>
         </thead>
         <tbody>
+        @foreach(Transaksi::all() as $transaksi)
         <tr>
-            <?php foreach(Transaksi::all() as $transkasi):?>
-            <?php 
-                echo $transaksi;
-                die();
-            ?>
             <td>{{$transaksi->id_transaksi}}</td>
             <td>{{$transaksi->id_putar}}</td>
             <td>{{$transaksi->id_user}}</td>
             <td>{{$transaksi->jumlah_tiket}}</td>
             <td>{{$transaksi->harga_total}}</td>
-            
-            <?php endforeach;?>
         </tr>
+        @endforeach
         </tbody>
     </table>
 @endsection
