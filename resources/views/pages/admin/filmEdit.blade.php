64ba031bc4dd66c2651b8bf2f6bba7e1f1bd4c77
durianpeople <durianpeople@gmail.com>
2018-12-17 17:32:01 +0700

    Update filmEdit.blade.php

--- resources/views/pages/admin/filmEdit.blade.php
+++ resources/views/pages/admin/filmEdit.blade.php
@@ -4,46 +4,34 @@
 <?php 
 use App\Film;
 use Illuminate\Support\Facades\Request;
-use PhpParser\Node\Stmt\Echo_;
 use Illuminate\Support\Facades\Storage;
 if ($mode=="edit")
 	$film=Film::find(Request::route('id'));
 ?>
-<?php
-if ($mode=="edit")
-	echo Form::model($film, [
-		'url' => '/admin/film/'.$film->id_film,
-		'files' => true,
-	]);
-else if ($mode=="add")
-	echo Form::open([
-		'url' => '/admin/film/add',
-		'files' => true,
-	]);
-
-echo Form::label('nama_film', 'Nama Film');
-echo Form::text('nama_film');
-echo "<br>";
-echo Form::label('tahun_pembuatan', 'Tahun Pembuatan');
-echo Form::number('tahun_pembuatan');
-echo "<br>";
-echo Form::label('durasi', 'Durasi');
-echo Form::number('durasi');
-echo "<br>";
-if ($mode=="edit") {
-	echo Form::label('image', 'Gambar');
-	echo Form::file('image');
-}
-else if ($mode=="add") {
-	echo "Tambahkan gambar dengan fitur edit";
-}
-?>
+<?php if ($mode=="edit"): ?>
+<form action="/admin/film/{{$film->id_film}}" method="post" enctype='multipart/form-data'>
+<?php elseif ($mode=="add"): ?>
+<form action="/admin/film/add" method="post" enctype='multipart/form-data'>
+<?php endif; ?>
+<label for="nama_film">Nama Film</label>
+<input type="text" name="nama_film" value="<?php echo ($mode=='edit')?$film->nama_film:'';?>"/>
+<br>
+<label for="tahun_pembuatan">Tahun Pembuatan</label>
+<input type="number" name="tahun_pembuatan" value="<?php echo ($mode=='edit')?$film->tahun_pembuatan:'';?>" />
+<br>
+<label for="durasi">Durasi</label>
+<input type="number" name="durasi" value="<?php echo ($mode=='edit')?$film->durasi:'';?>" />
+<br>
+<?php if ($mode=="edit"): ?>
+<label for="image">Gambar</label>
+<input type="file" name="image" />
+<?php elseif ($mode=="add"): ?>
+Tambahkan gambar dengan fitur edit
+<?php endif; ?>
 <?php if($mode=="edit"):?>
 <img src="/uploads/film_art/{{$film->id_film}}.png" />
 <?php endif;?>
-<?php
-echo "<br>";
-echo Form::submit();
-echo Form::close();
-?>
+<br>
+<input type="submit" value="Submit" />
+</form>
 @endsection
